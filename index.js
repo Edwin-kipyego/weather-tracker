046f7ef03efa99bfe74be23f9e3a42d0125eda2b
Edwin kipyego <koriredwin95@gmail.com>
2025-03-27 14:43:44 +0300

    fetch api data

--- index.js
+++ index.js
@@ -11,7 +11,36 @@ const city = document.querySelector(".search-box input").value;
 if (city == '')
   return;
  
-fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`).then(response => response.json()).then(json ==> {
+  const image = document.querySelector(.'weather-box img');
+  const temperature = document.querySelector(".weather-box .temperature");
+  const description = document.querySelector(".weather-box .description");
+  const humidity = document.querySelector('weather-details .humidity span);')
+  const wind = document.querySelector('.weather-details .wind span');
+
+  switch (json.weather[0].main) {
+    case 'Clear':
+      image.src = 'images/clear.png';
+      break;
+    case 'Rain':
+      image.src = 'images/rain.png';
+      break;
+    case 'Snow':
+      image.src = 'images/snow.png';
+      break;
+    case 'Clouds':
+      image.src = 'images/cloud.png';
+      break;
+    case 'Haze':
+      image.src = 'images/mist.png';
+      break;
+      
+      default;
+      image.src = 'images/clear.png';
+  }
+
+
+});
 
 });
 
